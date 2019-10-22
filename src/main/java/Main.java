f077e5da9b9c2e04a4f8a39871f1fb95e6971450
Boris Bovin <b.bovin@mail.ru>
2019-10-22 23:04:33 +0300

    added time measurement, added the ability to specify the number of threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,25 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Введите адрес сайта: пример - > https://yandex.ru ");
     Scanner sc = new Scanner(System.in);
+
+    System.out.println("Введите адрес сайта: пример - > https://yandex.ru ");
     String url = sc.nextLine();
 
+    System.out
+        .println("Введите количество создаваемых потоков: - > 0 установит значение по умолчанию.");
+    int numThreads = sc.nextInt();
+
     System.out.println("Сканируем сайт...");
+    long start = System.currentTimeMillis();
     LinkExecutor linkExecutor = new LinkExecutor(url, url);
-    String siteMap = new ForkJoinPool().invoke(linkExecutor);
+    String siteMap = numThreads == 0 ? new ForkJoinPool().invoke(linkExecutor)
+        : new ForkJoinPool(numThreads).invoke(linkExecutor);
 
     System.out.println("Сканирование завершено...");
+    System.out
+        .println("Время сканирования " + ((System.currentTimeMillis() - start) / 1000) + " сек.");
     writeFiles(siteMap);
-
   }
 
   private static void writeFiles(String map) {
